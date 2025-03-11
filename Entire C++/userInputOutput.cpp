66117d1951683c228fc9735b189392ad7c219f2c
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2025-03-11 18:37:23 +0530

    Add files via upload

---
+++ Entire C++/userInputOutput.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h> // It will import all the necessary library automaticaly
+using namespace std;
+int main(){
+    // for output we use cout
+    cout<<"Pranav"<<"\n";
+    cout<<"Pranav"<<endl;
+    // for input we use cin
+    cout<<"Enter the value of x: ";
+    int x,y;
+    cin>>x;
+    cout<<"Enter the value of y: ";
+    cin>>y;
+    cout<<"The value of x is: "<<x<<"\nand the value of y is: "<<y;
+    return 0;
+}
